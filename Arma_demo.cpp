98f0eafdda61ffc0287cf5ca1767e458bc77bb86
DimasYoga <dimasyogapra@gmail.com>
2019-07-14 18:24:58 +0700

    working

--- Arma_demo.cpp
+++ Arma_demo.cpp
@@ -113,7 +113,7 @@ arma::mat softmax_d(arma::mat input) {
 	double sum;
 	for (arma::mat::iterator it = out.begin(); it != it_end; ++it) {
 		arma::mat::iterator it2_end = input.end();
-		double sum = 0;
+		sum = 0;
 		for (arma::mat::iterator it2 = input.begin(); it2 != it2_end; ++it2) {
 			if (*it2 != *it) {
 				sum += *it2;
@@ -132,9 +132,9 @@ arma::mat quadraticLoss(arma::mat out, arma::mat goal) {
 	arma::mat::iterator it2 = goal.begin();
 
 	for (arma::mat::iterator it = cost.begin(); it != it_end; ++it) {
-		*it = pow(2, (*it2 - *it));
-		it1++;
-		it2++;
+		*it = pow((*it2 - *it1), 2);
+		++it1;
+		++it2;
 	}
 	return cost;
 }
@@ -146,29 +146,16 @@ arma::mat quadraticLoss_d(arma::mat out, arma::mat goal) {
 	arma::mat::iterator it2 = goal.begin();
 
 	for (arma::mat::iterator it = derivatif.begin(); it != it_end; ++it) {
-		++it1; ++it2;
 		*it = 0.5 * (*it2 - *it1);
+		++it1;
+		++it2;
 	}
 	return derivatif;
 }
 
-arma::mat calculateLayer(arma::mat input, arma::mat W, char select) {
-	arma::mat output(input.n_rows, W.n_cols, arma::fill::zeros);
-	
-	if (select == 'r') { //ReLu
-		output = relu(input * W);
-	}
-	else if (select == 's') { //Sigmoid
-		output = sigmoid(input * W);
-	}
-	else if (select == 'm') { //softmax
-		output = softmax(input * W);
-	}
-	else {
-		return NULL;
-	}
+arma::mat calculateLayer(arma::mat input, arma::mat W, arma::mat bias) {
 
-	return output;
+	return ((input * W) + bias);
 }
 
 void backProp(arma::mat error, arma::mat *W, char sel) {
@@ -177,44 +164,87 @@ void backProp(arma::mat error, arma::mat *W, char sel) {
 
 int main()
 {
-	float error = 0.0;
+	float error = 0;
+	float learn_rate = 0.3;
+	long long int iteration = 0;
 	arma::mat inp(1, 3, arma::fill::randu);
 	arma::mat l1(1, 2, arma::fill::zeros);
-	arma::mat l2(1, 3, arma::fill::zeros);
-	arma::mat out(1, 2, arma::fill::zeros);
-	arma::mat goal(out.size, arma::fill::randu);
-	arma::mat loss(out.size, arma::fill::zeros);
+	arma::mat l2(1, 4, arma::fill::zeros);
+	arma::mat out(1, 7, arma::fill::zeros);
+	arma::mat goal(arma::size(out), arma::fill::randu);
+	arma::mat loss(arma::size(out), arma::fill::zeros);
 
 	arma::mat w1(inp.n_cols, l1.n_cols, arma::fill::randu);
 	arma::mat w2(l1.n_cols, l2.n_cols, arma::fill::randu);
 	arma::mat w3(l2.n_cols, out.n_cols, arma::fill::randu);
 
-	arma::mat bias_l1(l1.size, arma::fill::ones);
-	arma::mat bias_l2(l2.size, arma::fill::ones);
-	arma::mat bias_out(out.size, arma::fill::ones);
+	arma::mat bias_l1(arma::size(l1), arma::fill::ones);
+	arma::mat bias_l2(arma::size(l2), arma::fill::ones);
+	arma::mat bias_out(arma::size(out), arma::fill::ones);
+
+	std::cout << "input :" << std::endl << inp << std::endl;
+	std::cout << "layer 1 :" << std::endl << relu(l1) << std::endl;
+	std::cout << "layer 2 :" << std::endl << sigmoid(l2) << std::endl;
+	std::cout << "output :" << std::endl << softmax(out) << std::endl;
+	std::cout << std::endl;
+	std::cout << "goal :" << std::endl << goal << std::endl;
+	std::cout << std::endl;
+	std::cout << "weight 1 :" << std::endl << w1 << std::endl;
+	std::cout << "weight 2 :" << std::endl << w2 << std::endl;
+	std::cout << "weight 3 :" << std::endl << w3 << std::endl;
+	std::cout << "bias layer 1 :" << std::endl << bias_l1 << std::endl;
+	std::cout << "bias layer 2 :" << std::endl << bias_l2 << std::endl;
+	std::cout << "bias layer output :" << std::endl << bias_out << std::endl;
+
+	system("pause");
 
 	do {
 		//forward propagation
-		l1 = calculateLayer(inp, w1, 'r') + bias_l1;
-		l2 = calculateLayer(l1, w2, 's') + bias_l2;
-		out = calculateLayer(l2, w3, 'm') + bias_out;
+		l1 = calculateLayer(inp, w1, bias_l1);
+		l2 = calculateLayer(relu(l1), w2, bias_l2);
+		out = calculateLayer(sigmoid(l2), w3, bias_out);
 
-		loss = quadraticLoss(out, goal);
+		loss = quadraticLoss(softmax(out), goal);
 		
 		arma::mat::iterator it_end = loss.end();
 
 		for (arma::mat::iterator it = loss.begin(); it != it_end; ++it) {
-			error += *it;
+			error += abs(*it);
 		}
 
-		error = error / loss.n_cols;
+		error = error / loss.n_elem;
+
+		std::cout << "Error : " << error << std::endl;
 
 		//back propagation
 
+		w3 += trans(sigmoid(l2)) * (quadraticLoss_d(softmax(out), goal) % softmax_d(out)) * learn_rate;
+		bias_out += quadraticLoss_d(softmax(out), goal) % softmax_d(out) * learn_rate;
 
+		w2 += (trans(relu(l1)) * sigmoid_d(l2)) * accu(quadraticLoss_d(softmax(out), goal) % softmax_d(out)) * learn_rate;
+		bias_l2 += sigmoid_d(l2) * accu(quadraticLoss_d(softmax(out), goal) % softmax_d(out)) * learn_rate;
 
+		w1 += (trans(inp) * relu_d(l1)) * accu(trans(sigmoid_d(l2)) * (quadraticLoss_d(softmax(out), goal) % softmax_d(out))) * learn_rate;
+		bias_l1 += relu_d(l1) * accu(trans(sigmoid_d(l2)) * (quadraticLoss_d(softmax(out), goal) % softmax_d(out))) * learn_rate;
+		iteration++;
+	} while (error > 0.000000002);
 
-	} while (error > 0.02);
+	std::cout << "input :" << std::endl << inp << std::endl;
+	std::cout << "layer 1 :" << std::endl << relu(l1) << std::endl;
+	std::cout << "layer 2 :" << std::endl << sigmoid(l2) << std::endl;
+	std::cout << "output :" << std::endl << softmax(out) << std::endl;
+	std::cout << std::endl;
+	std::cout << "goal :" << std::endl << goal << std::endl;
+	std::cout << std::endl;
+	std::cout << "weight 1 :" << std::endl << w1 << std::endl;
+	std::cout << "weight 2 :" << std::endl << w2 << std::endl;
+	std::cout << "weight 3 :" << std::endl << w3 << std::endl;
+	std::cout << "bias layer 1 :" << std::endl << bias_l1 << std::endl;
+	std::cout << "bias layer 2 :" << std::endl << bias_l2 << std::endl;
+	std::cout << "bias layer output :" << std::endl << bias_out << std::endl;
+
+	std::cout << "Error : " << error << std::endl;
+	std::cout << "Iteration : " << iteration << std::endl;
 
 	return 0;
 }
